1b0659cfcfec40312abf79ab37f0d11e6e0668ea
kuhndev <whyyoutrybro@gmail.com>
2021-12-13 22:00:49 -0500

    various additions

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+var PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
+
+app.listen(PORT, () => console.log("Server is running on " + PORT));
